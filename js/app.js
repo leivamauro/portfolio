ce9302723d2dc427f3f0f3a53c9213b22e72f1e6
Mauro Window <mlemauro02@gmail.com>
2023-03-24 22:18:38 -0300

    mejorado responsive

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
+/* CONFIGURACION MODO OSCURO -- INICIO */
 const darkModeBtn = document.getElementById('dark-mode-btn');
 const body = document.querySelector('body');
 const switchBtn = document.getElementById('dark-mode-btn');
 
 let darkMode = true;
 
+if (darkMode) {    
+  darkMode = false;
+} else {
+  darkMode = true;
+} 
+
 darkModeBtn.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
-
-  if (darkMode) {
-    switchBtn.style = 'display: flex; justify-content: end; background-color: #ffffffb3;';
-    darkMode = false;
-  } else {
-    switchBtn.style = 'display: flex; justify-content: start; background-color: #132029b3;';
-    darkMode = true;
-  }
 });
+/* CONFIGURACION MODO OSCURO -- FIN */
 
