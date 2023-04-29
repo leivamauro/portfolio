589073c06e2826605890aa9801f17b6537cd5733
Mauro Window <mlemauro02@gmail.com>
2023-04-28 22:14:53 -0300

    agregado efecto de escribir js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+
+
+
 /* CONFIGURACION MODO OSCURO -- INICIO */
 const darkModeBtn = document.getElementById('dark-mode-btn');
 const body = document.querySelector('body');
@@ -16,3 +19,32 @@ darkModeBtn.addEventListener('click', () => {
 });
 /* CONFIGURACION MODO OSCURO -- FIN */
 
+escribir('Sobre mi', "about_me_title");
+escribir('Proyectos Academicos', "portfolio_title");
+escribir('Habilidades Tecnicas', "skills_title");
+escribir('Mis estudios', "studies_title");
+
+// ---- FUNCION efecto de escribir ----
+function escribir(textoAEscribir, elementoID) {
+
+    let texto = document.getElementById(elementoID);
+    let arrayCaracteres = textoAEscribir.split('');
+    let indice = 0;
+
+    const intervalID = setInterval(() => {    
+        
+        if (indice >= arrayCaracteres.length) {
+            
+            clearInterval(intervalID);
+            setTimeout(() => {//se espera un tiempo de 3 seg para entrar a limpiar el arreglo y llamar la recursividad
+                texto.textContent = '';
+                escribir(textoAEscribir, elementoID);
+            }, 2500);
+
+        } else {  
+            texto.textContent += arrayCaracteres[indice];
+            indice++;
+        }
+
+    }, 200); // 1000ms = 1s 
+}
